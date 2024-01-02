12e12ed74f4ba3835b5f5e3613d524d271978c9b
abhishek-sde-singh <154674165+abhishek-sde-singh@users.noreply.github.com>
2024-01-02 16:27:51 +0530

    Update WebWorker.js

--- src/WebWorker.js
+++ src/WebWorker.js
@@ -1,7 +1,10 @@
 export default class WebWorker {
     constructor(worker) {
+    // Convert the provided worker function to a string
         const code = worker.toString();
+    // Create a Blob containing the worker code
         const blob = new Blob(['('+code+')()']);
+    // Create a new Web Worker using the Blob's URL
         return new Worker(URL.createObjectURL(blob));
     }
 }
